409536a0601322dfbc573e1d15e3ef4ec00d997e
Anani1994 <dongwhchn@163.com>
2019-03-31 18:21:46 +0800

    实现轮播图无缝循环

--- src/components/slide-show/components/SlideShow.vue
+++ src/components/slide-show/components/SlideShow.vue
@@ -2,21 +2,23 @@
     .slide-container
         button.arrow(
             type="button"
-            @click="toLeft()"
+            @click="move(1)"
         )
             font-awesome-icon(:icon="['fas', 'angle-double-left']")
         .slideTrack.active(
             ref="slideTrack"
             :style="trackStyles"
+            :class="[active ? '' : 'active']"
         )
             slot
         .slideTrackBak(
             ref="slideTrackBak"
             :style="trackBakStyles"
+            :class="[active ? 'active' : '']"
         )
         button.arrow(
             type="button"
-            @click="toRight()"
+            @click="move(-1)"
         )
             font-awesome-icon(:icon="['fas', 'angle-double-right']")
         ul.dots
@@ -70,11 +72,14 @@ export default {
   },
   data() {
     return {
-      current: 0, // 当前显示元素的索引
       childWidth: 0, // 每个子元素的宽度
       childLen: 0, // 实际子元素个数
       trackWidth: 0, // 轨道宽度
+      current: 0, // 当前显示元素的索引
+      currentBak: 4, // 第二轨道当前显示元素的索引
       trackOffset: 0, // 轨道偏移量
+      trackBakOffset: 0, // 第二轨道偏移量
+      active: false, // 第二轨道在上，改变样式遮盖复位
       timer: null // 计时器
     };
   },
@@ -88,32 +93,52 @@ export default {
       );
       this.$refs.slideTrackBak.innerHTML = this.$refs.slideTrack.innerHTML; // 复制第一轨道，以实现无缝循环
     },
-    toLeft() {
+    move(step = 1) {
+      // 初始化轨道位置
+      if (0 < step) {
+        if (this.active) {
+          this.trackOffset = -this.childWidth * step;
+        } else {
+          this.trackBakOffset = -this.childWidth * step;
+        }
+      } else {
+        if (this.active) {
+          this.trackOffset = this.childWidth * this.childLen;
+        } else {
+          this.trackBakOffset = this.childWidth * this.childLen;
+        }
+      }
+      // 边界处理
+      if ((step > 0 && this.childLen - 1 === this.current) || (step < 0 && 0 === this.current)) {
+        this.active = !this.active;
+        this.trackOffset += this.childWidth * step;
+        this.trackBakOffset += this.childWidth * step;
+      } else {
+        if (this.active) {
+          this.trackBakOffset += this.childWidth * step;
+        } else {
+          this.trackOffset += this.childWidth * step;
+        }
+      }
+      this.current += step;
+      if (this.current === this.childLen) this.current = 0;
+      if (this.current < 0) this.current += this.childLen;
+
+      this.$emit('on-change', this.current);
       this.setAutoplay();
-      this.current += 1;
-      this.move();
-    },
-    toRight() {
-      this.setAutoplay();
-      this.current -= 1;
-      this.move();
-    },
-    move(offset) {
-      this.trackOffset =
-        this.childWidth * (offset || Math.abs(this.current) % this.childLen);
     },
     // 圆点点击事件
     toDesignated(index) {
-      this.trackOffset = this.childWidth * index;
+      this.active ? this.trackBakOffset = this.childWidth * index : this.trackOffset = this.childWidth * index;
       this.current = index;
-      // 重置计时器
-      this.setAutoplay();
+      this.setAutoplay(); // 重置计时器
     },
+    // 设置轮播
     setAutoplay() {
       window.clearInterval(this.timer);
       if (this.autoplay) {
         this.timer = window.setInterval(() => {
-          this.toLeft();
+          this.move();
         }, this.autoplaySpeed);
       }
     }
@@ -123,6 +148,7 @@ export default {
     this.setAutoplay();
   },
   computed: {
+    // 当下面对应依赖值改变时重置动画
     trackStyles() {
       return {
         width: `${this.trackWidth}px`,
@@ -133,7 +159,7 @@ export default {
     trackBakStyles() {
       return {
         width: `${this.trackWidth}px`,
-        transform: `translate3d(${-this.trackOffset}px, 0px, 0px)`,
+        transform: `translate3d(${-this.trackBakOffset}px, 0px, 0px)`,
         transition: `transform 500ms ${this.easing}`
       };
     }

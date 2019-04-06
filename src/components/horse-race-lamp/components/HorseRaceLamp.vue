925ba58aade0a318312133922fabe485b612739e
Anani1994 <dongwhchn@163.com>
2019-04-07 00:08:33 +0800

    JavaScript动态设置动画改进跑马灯的实现

--- src/components/horse-race-lamp/components/HorseRaceLamp.vue
+++ src/components/horse-race-lamp/components/HorseRaceLamp.vue
@@ -2,32 +2,131 @@
     .horse-race-lamp-container
         .horse-race-lamp(
             ref="horseRaceLamp"
-            style="styles"
+            :style="styles"
         )
             slot
 </template>
 
 <script>
 import dom from "../../../libs/utils/dom";
+import common from "../../../libs/utils/common";
+import utils from "../../../libs/util";
+import event from "../../../libs/utils/event";
+import perf from "../../../libs/utils/performance";
 export default {
+  props: {
+    delay: {
+      type: Number, // 单位毫秒
+      default: 0
+    },
+    timeout: {
+      type: Number, // 单位毫秒
+      default: 5000
+    },
+    timingFunction: {
+      type: String,
+      default: 'linear',
+      validator(value) {
+        const regexp = /^(cubic-bezier\()([01]\.?\d*,?){4}\)$/;
+        return ['linear', 'ease', 'ease-in', 'ease-out', 'ease-in-out'].includes(value) ||
+          regexp.test(value);
+      }
+    },
+    iterationCount: {
+      type: [String, Number],
+      default: 'infinite',
+      validator(value) {
+        if (utils.isString(value)) {
+          if ("infinite" === value) return true;
+          return false;
+        }
+        return true;
+      }
+    },
+    direction: {
+      type: String,
+      default: 'normal',
+      validator(value) {
+        return ['normal', 'reverse', 'alternate', 'alternate-reverse', 'initial', 'inherit'].includes(value);
+      }
+    },
+    playState: {
+      type: String,
+      default: 'running',
+      validator(value) {
+        return ['paused', 'running'].includes(value);
+      }
+    },
+    fillMode: {
+      type: String,
+      default: 'none',
+      validator(value) {
+        return ['none', 'forwards', 'backwards', 'both', 'initial', 'inherit'].includes(value);
+      }
+    }
+  },
   data() {
     return {
-      styles: null,
     }
   },
   methods: {
-    setSlideWidth() {
+    setSlide() {
       const slideContainerWidth = dom.getStyle(this.$el, 'width');
-      const slideWidth = dom.getStyle(this.$refs.horseRaceLamp, 'width');
-      slideWidth > slideContainerWidth ? this.$el.style.width = slideWidth : this.$refs.horseRaceLamp.style.display = "block";
+      const nodeList = this.$refs.horseRaceLamp.children;
+      if (nodeList.length > 1) {
+        console.warn('HorseRaceLamp should contain exactly one root element.');
+      }
+      const slideWidth = dom.getStyle(nodeList[0], 'width');
+
+      const frameRule = dom.getFrameRule('HorseRaceLamp');
+      const { keys } = dom.getFrameKey(frameRule);
+      const rules = [];
+
+      this.clearFrameRule(frameRule, keys);
+      if (slideWidth > slideContainerWidth) {
+        rules.push(`0% {transform: translateX(-${slideWidth});}`);
+        rules.push(`100% {transform: translateX(${slideWidth});}`);
+      } else {
+        rules.push(`0% {transform: translateX(-${slideWidth});}`);
+        rules.push(`100% {transform: translateX(${slideContainerWidth});}`);
+      }
+
+      this.addRule(frameRule, rules);
     },
-    move() {
-      this.$refs.horseRaceLamp.style.animation = 'HorseRaceLamp 5s linear 0s infinite';
+    /**
+     * 清除动画规则
+     * @param {*} rule 动画规则
+     * @param {*} keys 动画
+     */
+    clearFrameRule(rule, keys) {
+      for (let i = 0; i < keys.length; i++) {
+        rule.deleteRule(keys[i]);
+      }
     },
+    addRule(frameRule, rules) {
+      rules.forEach(rule => {
+        frameRule.appendRule(rule);
+      });
+    },
+    handleResize() {
+      perf.debounce(this.setSlide, this);
+    }
   },
   mounted() {
-    this.setSlideWidth();
-    this.move();
+    this.setSlide();
+    event.addHandler(window, "resize", this.handleResize);
+  },
+  computed: {
+    styles() {
+      const styles = {
+        animation: ` HorseRaceLamp ${this.timeout}ms ${this.timingFunction} ${this.delay}ms ${this.iterationCount} ${this.direction} ${this.fillMode} ${this.playState}`
+      };
+
+      return styles;
+    },
+  },
+  beforeDestroy() {
+    event.removeHandler(window, "resize", this.handleResize);
   },
 }
 </script>
--- src/libs/utils/dom.js
+++ src/libs/utils/dom.js
@@ -18,4 +18,47 @@ export default new class {
       return ele.style[styleName];
     }
   }
+
+  /**
+   * 查找指定动画的规则
+   * @param rule {string} 由 @keyframes 指定的动画名称
+   * @returns {object|null}
+   */
+  getFrameRule(rule) {
+    const styleSheets = document.styleSheets;
+    for (let i = 0; i < styleSheets.length; i++) {
+      for (let j = 0; j < styleSheets[i].cssRules.length; j++) {
+        if (styleSheets[i].cssRules[j].type === window.CSSRule.KEYFRAMES_RULE &&
+          styleSheets[i].cssRules[j].name === rule) {
+          return styleSheets[i].cssRules[j];
+        }
+      }
+    }
+    return null;
+  }
+
+  /**
+   * 获取指定动画规则的 key 值
+   * @param {*} keyframesRule 动画规则
+   * @returns {array} 动画的键名数组
+   */
+  getFrameKey(keyframesRule) {
+    const keys = [];
+    const cssRules = keyframesRule.cssRules;
+    const len = cssRules.length;
+    for (var i = 0; i < len; i++) {
+      keys.push(cssRules[i].keyText);
+    }
+
+    // Removes all the % values from the array so
+    // the getClosest function can perform calculations
+    const nums = keys.map(function (str) {
+      return str.replace('%', '');
+    });
+
+    return {
+      keys,
+      nums
+    };
+  }
 }();

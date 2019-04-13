cfb3338a8f61dda2860fd9297af2e92b12ea26a8
Anani1994 <dongwhchn@163.com>
2019-04-13 23:53:53 +0800

    构建百叶窗功能的基本思路

--- src/components/window-blinds/components/WindowBlinds.vue
+++ src/components/window-blinds/components/WindowBlinds.vue
@@ -1,29 +1,95 @@
 <template lang="pug">
-    ul.window-blinds.string
-        li.string-wrap(v-for="num in leafNumber")
-            .string-content(:class="[current <= num ? 'up' : 'down']")
-                .string 第{{num + 1}}行！
-                .string-bak 第{{num}}行！
+    ul.window-blinds(
+        :class="mode"
+        @click="toggleBlinds"
+    )
+        li.string-wrap(
+            v-for="(num, index) in leafNumber"
+            :class="[{open: openBlids && 'blinds' === mode}]"
+        )
+            .string-content(
+                :style="{top: current > index ? `-${getTop(index, true)}px` : `-${getTop(index)}px`}"
+                class="string-content"
+                :class="transition"
+            )
+                .string(v-for="(text, index) in text") {{text[index]}}
 </template>
 
 <script>
+import common from '../../../libs/utils/common';
+import poetry from './text';
 export default {
+    props: {
+        mode: {
+            type: String,
+            default: 'string'
+        },
+        text: {
+            type: String,
+            default: ''
+        }
+    },
     data() {
         return {
-            leafNumber: 25,
+            openBlids: false,
             i: 0,
+            n: 0,
             current: 0,
+            lineNum: 3,
+            leafNumber: 5,
+            lineHeight: 24,
+            transition: 'transition',
+            tid: null,
+            textArr: ''
         };
     },
     mounted() {
-        setTimeout(this.open, 1000);
+        this.init();
+        this.move();
     },
     methods: {
-        open() {
-            this.current = this.leafNumber;
+        init() {
+            this.textArr = common.getStringArr(poetry, 'poetry');
+            console.log(this.textArr);
+            this.textArr.length += this.textArr.length % this.lineNum;
+            this.textArr.fill();
         },
-        close() {
+        toggleBlinds() {
+            this.openBlids = !this.openBlids;
+        },
+        getTop(index, next) {
+            let topPos = '';
+            if (next) {
+                topPos = (this.lineHeight * (this.lineNum - this.n - 1)).toString().replace(/-/, '');
+            } else {
+                topPos = (this.lineHeight * (this.lineNum - this.n)).toString().replace(/-/, '');
+            }
+            return topPos;
+        },
+        move() {
             this.current = 0;
+            this.i = 0;
+            this.n++;
+            this.tid = setInterval(() => {
+                this.i++;
+                this.current += this.i;
+                if (this.current > this.leafNumber) {
+                    clearInterval(this.tid);
+                    if (this.n < this.lineNum - 1) {
+                        setTimeout(() => {
+                            this.move();
+                        }, 1000); // 每次滑动到底时的停留时间
+                    } else {
+                        // 第一轮滑动完成的边界处理
+                        setTimeout(() => {
+                            this.n = 0;
+                            setTimeout(() => {
+                                this.move();
+                            }, 1000);
+                        }, 1000);
+                    }
+                }
+            }, 500);
         }
     },
 };
--- src/libs/utils/common.js
+++ src/libs/utils/common.js
@@ -1,18 +1,42 @@
 export default new class {
-  /**
-   * 对传入的值与最大值和最小值进行比较
-   * @parmas {number} 传入的值
-   * @parmas {number} 最小值
-   * @parmas {number} 最大值
-   * @returns {number} 大于最大取最大，小于最小取最小，否则返回原值
-   */
-  clamp(value, min = 0, max = 0) {
-    if (value < min) {
-      return min;
+    /**
+     * 对传入的值与最大值和最小值进行比较
+     * @parmas {number} 传入的值
+     * @parmas {number} 最小值
+     * @parmas {number} 最大值
+     * @returns {number} 大于最大取最大，小于最小取最小，否则返回原值
+     */
+    clamp(value, min = 0, max = 0) {
+        if (value < min) {
+            return min;
+        }
+        if (value > max) {
+            return max;
+        }
+        return value;
     }
-    if (value > max) {
-      return max;
+
+    /**
+     * @description
+     * 在理想情况下对传入的字符串进行包括分隔符的分隔
+     * @issue
+     * 字符串以分隔符开头？
+     * 字符串不以分隔符结束?
+     * 英文状态下的单双引号?
+     * @param {string} str 需要被分割的字符串
+     * @returns {array} 分割后的字符串，包含分隔符
+     */
+    getStringArr(str = '', mode = 'string') {
+        if (mode === 'poetry') return str.trim().split(/\s/);
+        const strArr = str.split(/[,\.\?!;，。！？]+(?![^\(（“‘)]*[\)）“‘])/);
+        const separators = str.match(/[,\.\?!;，。！？]+(?![^\(“‘)]*[\)“‘])/g);
+        // 去掉以分隔符结束生成的空元素
+        if (strArr[strArr.length - 1] === '') strArr.length--;
+        if (!separators) return strArr;
+        return strArr.map((item, index) => {
+            let str = item;
+            str += separators[index];
+            return str;
+        });
     }
-    return value;
-  }
 }();
